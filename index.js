1eccbba0507c7c4151b17e65f1498d73e81cb1d7
Samyak Sukhdeve <samyaksukhadeve12@gmail.com>
2025-01-15 19:48:25 +0530

    cors fix

--- index.js
+++ index.js
@@ -7,10 +7,20 @@ const app = express();
 const port = process.env.PORT;
 app.use(express.json());
 
-const corsOptions = {
-  origin: process.env.ALLOWED_ORIGIN,
-};
-app.use(cors(corsOptions));
+const allowedOrigins = [process.env.ALLOWED_ORIGIN];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true); // Allow request
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 app.get("/test", (req, res) => {
   try {
